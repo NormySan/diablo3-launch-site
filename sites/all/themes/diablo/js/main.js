71584629d0263aa8f82c392eb33a508abf468349
Christoffer Palm <christoffer.palm@oddhill.se>
2014-04-03 10:43:46 +0200

    Added diablo 3 theme.

---
+++ sites/all/themes/diablo/js/main.js
@@ -0,0 +1,15 @@
+(function($) {
+
+// Run whenever the DOM tree is changed, e.g. through AJAX/AHAH
+Drupal.behaviors.DIABLO = {
+  attach: function (context, settings) {
+
+  }
+}
+
+// Run once when the DOM is ready (page load)
+$(document).ready(function() {
+
+});
+
+})(jQuery);
