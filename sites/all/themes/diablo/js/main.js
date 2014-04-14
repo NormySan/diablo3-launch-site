5d82972cc8956b540ee42623f10639f35973aada
Christoffer Palm <christoffer.palm@oddhill.se>
2014-04-14 18:58:36 +0200

    Style changes.

--- sites/all/themes/diablo/js/main.js
+++ sites/all/themes/diablo/js/main.js
@@ -10,7 +10,7 @@ Drupal.behaviors.diablo = {
   	$('#frontpage-slider').owlCarousel({
 		navigation : true,
       	slideSpeed : 300,
-      	paginationSpeed : 400,
+      	pagination: false,
       	singleItem:true,
       	navigationText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
       	theme: 'frontpage-slider'
